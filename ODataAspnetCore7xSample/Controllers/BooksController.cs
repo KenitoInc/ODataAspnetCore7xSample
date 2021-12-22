05da297494b87c53fbe99c60e3003a597ced08be
Kennedy Kangethe <kemunga@microsoft.com>
2021-12-22 18:05:59 +0300

    Add project files.

---
+++ ODataAspnetCore7xSample/Controllers/BooksController.cs
@@ -0,0 +1,128 @@
+﻿using System.Linq;
+using Microsoft.AspNet.OData;
+using Microsoft.AspNet.OData.Query;
+using Microsoft.AspNetCore.Mvc;
+using ODataAspnetCore7xSample.Models;
+
+namespace ODataAspnetCore7xSample.Controllers
+{
+    public class BooksController : ODataController
+    {
+        // Get ~/Books
+        [EnableQuery(AllowedQueryOptions = AllowedQueryOptions.All, MaxTop = 1, PageSize = 100, MaxExpansionDepth = 5)]
+        public IQueryable<Book> Get()
+        {
+            return DataSource.Instance.Books.AsQueryable<Book>();
+        }
+
+        // GET ~/Books(1)
+        [EnableQuery]
+        public SingleResult<Book> Get(int key)
+        {
+            IQueryable<Book> result = DataSource.Instance.Books.AsQueryable<Book>().Where(b => b.ID == key);
+            return SingleResult.Create(result);
+        }
+
+        // PUT ~/Books(1)
+        [EnableQuery]
+        public IActionResult Put(int key, [FromBody] Book book)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var entity = DataSource.Instance.Books.Where(b => b.ID == key);
+            if (entity == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(entity);
+        }
+
+        // PATCH ~/Books(1)
+        [EnableQuery]
+        public IActionResult Patch([FromODataUri] int key, Delta<Book> delta)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var book = DataSource.Instance.Books.Where(b => b.ID == key);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(book);
+        }
+
+        // DELETE ~/Books(1)
+        [EnableQuery]
+        public IActionResult Delete(int key)
+        {
+            var book = DataSource.Instance.Books.Where(b => b.ID == key);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(book);
+        }
+
+        [EnableQuery]
+        public IActionResult GetMainAuthor([FromODataUri] int key)
+        {
+            var mainAuthor = DataSource.Instance.Books.Where(m => m.ID == key).Select(m => m.MainAuthor).Single();
+            return Ok(mainAuthor);
+        }
+
+        // GET /Books/MostRecent()
+        // This is a bound function. It's bound to the entity set Books.
+        public IActionResult MostRecent()
+        {
+            var product = DataSource.Instance.Books.Max(x => x.ID);
+            return Ok(product);
+        }
+
+        // POST /Books(1)/Rate
+        // Body has { Rating: 7 }
+        // This is bound Action. The action is bound to the Books entity set.
+        public IActionResult Rate([FromODataUri] int key, ODataActionParameters parameters)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            int rating = (int)parameters["Rating"];
+
+            if (rating < 0)
+            {
+                return BadRequest();
+            }
+
+            return Ok(new BookRating() { BookID = key, Rating = rating });
+        }
+
+        // GET ReturnAllForKidsBooks()
+        // This is an unbound Function.
+        [HttpGet("/odata/ReturnAllForKidsBooks()")]
+        public IActionResult ReturnAllForKidsBooks()
+        {
+            var forKidsBooks = DataSource.Instance.Books.Where(m => m.ForKids == true);
+            return Ok(forKidsBooks);
+        }
+
+        // GET ~/Books(1)/Authors
+        // Authors is a navigation property.
+        [EnableQuery]
+        public IActionResult GetAuthors([FromODataUri] int key)
+        {
+            var result = DataSource.Instance.Books.AsQueryable<Book>().Where(b => b.ID == key).Select(b => b.Authors);
+            return Ok(result);
+        }
+    }
+}
---
+++ ODataAspnetCore7xSample/Models/DataSource.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ODataAspnetCore7xSample.Models
+{
+    /// <summary>
+    /// In memory data store.
+    /// Opted for this instead of EFCore since EFCore doesn't work well with Complex Types.
+    /// </summary>
+    public class DataSource
+    {
+        private static DataSource instance = null;
+        DateTime date1 = new DateTime(2008, 5, 1, 8, 30, 52);
+        public static DataSource Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new DataSource();
+                }
+                return instance;
+            }
+        }
+        public List<Book> Books { get; set; }
+        public List<Author> Authors { get; set; }
+        public List<Publisher> Publishers { get; set; }
+        public List<Address> Addresses { get; set; }
+
+        private DataSource()
+        {
+            this.Reset();
+            this.Initialize();
+        }
+        public void Reset()
+        {
+            this.Books = new List<Book>();
+            this.Authors = new List<Author>();
+            this.Publishers = new List<Publisher>();
+            this.Addresses = new List<Address>();
+        }
+        public void Initialize()
+        {
+            this.Addresses.AddRange(new List<Address>()
+            {
+                new Address(){ County = "County 1", Town = "Town 1"},
+                new Address(){ County = "County 2", Town = "Town 2"},
+                new Address(){ County = "County 3", Town = "Town 3"},
+                new Address(){ County = "County 4", Town = "Town 4"},
+                new Address(){ County = "County 5", Town = "Town 5"},
+            });
+
+            this.Authors.AddRange(new List<Author>()
+            {
+                new Author(){ID = 10001, AuthorName = "Author 1", Addresses = new List<Address>(){Addresses[0], Addresses[1], Addresses[4]}},
+                new Author(){ID = 10002, AuthorName = "Author 2", Addresses = new List<Address>(){Addresses[1], Addresses[2], Addresses[4]}},
+                new Author(){ID = 10003, AuthorName = "Author 3", Addresses = new List<Address>(){Addresses[2], Addresses[3]}},
+                new Author(){ID = 10004, AuthorName = "Author 4", Addresses = new List<Address>(){Addresses[3], Addresses[4]}},
+                new Author(){ID = 10005, AuthorName = "Author 5", Addresses = new List<Address>(){Addresses[0], Addresses[3], Addresses[2]}},
+            });
+
+            this.Publishers.AddRange(new List<Publisher>()
+            {
+                new Publisher(){ID = 1001, Address = Addresses[2], PublisherName = "Publisher 1", Authors = new List<Author>(){Authors[0], Authors[1], Authors[3]}},
+                new Publisher(){ID = 1002, Address = Addresses[4], PublisherName = "Publisher 2", Authors = new List<Author>(){Authors[1], Authors[2]}},
+                new Publisher(){ID = 1003, Address = Addresses[0], PublisherName = "Publisher 3", Authors = new List<Author>(){Authors[3], Authors[1]}},
+                new Publisher(){ID = 1004, Address = Addresses[1], PublisherName = "Publisher 4", Authors = new List<Author>(){Authors[4], Authors[2]}},
+                new Publisher(){ID = 1005, Address = Addresses[3], PublisherName = "Publisher 5", Authors = new List<Author>(){Authors[0], Authors[4], Authors[2]}},
+            });
+
+            this.Books.AddRange(new List<Book>()
+            {
+                new Book(){ID = 1, Isbn = "AA0011", Title = "Book 1", Year = 2000, ForKids = false, Authors = new List<Author>(){Authors[0], Authors[1]}},
+                new Book(){ID = 2, Isbn = "BB0011", Title = "Book 2", Year = 2001, ForKids = true, Authors = new List<Author>(){Authors[4], Authors[1], Authors[2]}},
+                new Book(){ID = 3, Isbn = "CC0011", Title = "Book 3", Year = 2002, ForKids = false, Authors = new List<Author>(){Authors[2], Authors[3]}},
+                new Book(){ID = 4, Isbn = "DD0011", Title = "Book 4", Year = 2003, ForKids = true, Authors = new List<Author>(){Authors[0], Authors[1], Authors[1]}},
+                new Book(){ID = 5, Isbn = "EE0011", Title = "Book 5", Year = 2004, ForKids = true, Authors = new List<Author>(){Authors[0], Authors[4], Authors[3]}},
+                new Book(){ID = 6, Isbn = "FF0011", Title = "Book 6", Year = 2005, ForKids = true, Authors = new List<Author>(){Authors[3], Authors[1]}},
+                new Book(){ID = 7, Isbn = "GG0011", Title = "Book 7", Year = 2006, ForKids = true, Authors = new List<Author>(){Authors[0], Authors[1], Authors[2]}},
+                new Book(){ID = 8, Isbn = "HH0011", Title = "Book 8", Year = 2007, ForKids = false, Authors = new List<Author>(){Authors[3], Authors[4]}},
+            });
+        }
+    }
+}
